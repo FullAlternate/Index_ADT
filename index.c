824f1301bb5f925650102b99c4d1bcf31d6a6023
FullAlternate <33181597+FullAlternate@users.noreply.github.com>
2018-04-23 04:00:36 +0900

    Add files via upload

--- index.c
+++ index.c
@@ -6,20 +6,21 @@
 
 struct index {
     map_t *map;
-    set_t *set;
 };
 
 /*
  * Creates a new, empty index.
  */
-index_t *index_create(cmpfunc_t cmpfunc, hashfunc_t hashfunc){
+index_t *index_create(){
     index_t *index = malloc(sizeof(index_t));
+    
     if (index == 0){
         fatal_error("out of memory");
     }
 
-    index->map = map_create(cmpfunc, hashfunc);
-    index->set = set_create(cmpfunc);
+    index->map = map_create(compare_strings, hash_string);
+
+    return index;
 }
 
 /*
@@ -28,7 +29,6 @@ index_t *index_create(cmpfunc_t cmpfunc, hashfunc_t hashfunc){
  */
 void index_destroy(index_t *index){
     map_destroy(index->map, NULL, NULL);
-    set_destroy(index->set);
     free(index);
 }
 
@@ -39,8 +39,23 @@ void index_destroy(index_t *index){
  *       'path' and the contents of the 'words' list.
  */
 void index_addpath(index_t *index, char *path, list_t *words){
-    set_add(index->set, words);
-    map_put(index->map, path, index->set->words);
+    set_t *tempset;
+    list_iter_t *words_iter = list_createiter(words);
+    void *tempword;
+    while(list_hasnext(words_iter) == 1){
+        tempword = list_next(words_iter);
+
+        if(map_haskey(index->map, tempword)){
+            tempset = map_get(index->map, tempword);       
+        } else {
+            tempset = set_create(compare_strings); 
+        }
+
+        map_put(index->map, tempword, tempset);
+        set_add(tempset, path);
+    }
+    //printf("%s", (char*)tempword);
+    //printf("%s", path);
 
 }
 
@@ -51,4 +66,11 @@ void index_addpath(index_t *index, char *path, list_t *words){
  * is assigned to the given errmsg pointer and the return value
  * will be NULL.
  */
-list_t *index_query(index_t *index, list_t *query, char **errmsg);
+list_t *index_query(index_t *index, list_t *query, char **errmsg){
+       /* list_t *alist = list_create(compare_strings);
+        void *banan = "banan";
+        list_addlast(alist, banan);
+        printf("%d", list_poplast(alist));
+        list_addlast(alist, banan);
+        return alist;*/
+}
